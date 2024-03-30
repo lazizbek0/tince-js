f6367a8cbca714377c59517f26155ffe220dbeda
Your Name <you@example.com>
2024-03-30 13:10:17 +0500

    kjhgf

--- js/script.js
+++ js/script.js
@@ -1,51 +1,82 @@
-window.addEventListener('DOMContentLoaded', () =>{
+  const navigation = document.querySelector(".navigation");
+  const navA = navigation.querySelectorAll("a");
 
 
-    const navigation = document.querySelector(".navigation");
-    const navA = navigation.querySelectorAll(" a");
-    const navBars = navigation.querySelector("i");
-    const bars = document.querySelector('.fa-bars');
-    
-    bars.addEventListener('click', () =>{
-      navigation.classList.toggle('height')
-    })
-    
-    
-    window.addEventListener("scroll", () => {
-      const scrollPosition = window.scrollY;
-      if (scrollPosition >= 100) {
-        navigation.classList.add("bgcWhite");
-    
-        navBars.style.color = "black";
-        navA.forEach((item) => {
-          item.style.color = "black";
-        });
-      } else {
-        navigation.classList.remove("bgcWhite");
-    
-        navBars.style.color = "white";
-        navA.forEach((item) => {
-          item.style.color = "white";
-        });
+  window.addEventListener('scroll' , () =>{
+    const scrollPosition = window.scrollY;
+    if(scrollPosition >= 100) {
+      navigation.classList.add("bgcWhite");
+
+  navA.forEach((item) => {
+    item.style.color = "block";
+  });
+
+
     }
-    document.addEventListener('keydown', e =>{
-      if(e.code == 'Escape'){
-        handleHideModal()
-      }
-    })
+
+    else{
+      navigation.classList.remove("bgcWhite");
+
+      navA.forEach((item) => {
+        item.style.color = "White";
+      });
+    }
+  });
+
+  // gallery
+const galleryAllTabs = document.querySelector('.gallery__tabs'),
+     galleryTab = document.querySelectorAll('.gallery__tab'),
+     galleryContent = document.querySelectorAll('.gallery__body');
     
-    document.addEventListener('keydown', e =>{
-      if(e.code == 'Insert'){
-        const isClose = confirm('saytni yopmoqchimsz');
-        if (isClose){
-          close()
-        }
-        else{
-          alert('saytdan foydalanishingiz mumkin')
-        }
-      }
-    })
 
-    })
 
-    })
+
+// contact yashirish
+
+function hideGalleryContent(){
+      galleryContent.forEach(item =>{
+        item.classList.add('hide');
+        item.classList.remove('show' , 'fade');
+      })
+      galleryTab.forEach(item =>{
+        item.classList.remove('active');
+      })
+     }
+
+
+   function showGalleryContent (i){
+   galleryContent[i].classList.add('show', 'fade');
+   galleryContent[i].classList.remove('hide');
+   galleryTab[i].classList.add('active');
+   }
+   hideGalleryContent();
+   showGalleryContent(3);
+
+
+  //  bosilganda ishga tushl
+
+  
+   galleryAllTabs.addEventListener('click', (e) =>{
+     const target = e.target;
+     if(target && target.classList.contains('gallery__tab')){
+      galleryTab.forEach((item, idx) =>{
+        if(target == item){
+        hideGalleryContent();
+        showGalleryContent(idx);
+        }
+      })
+     }
+   })
+
+
+
+
+   const swiper = new Swiper('.swiper', {
+
+     loop: true,
+     autoplay:{
+      delay: 2000,
+      disableOnItaraction: false,
+    },
+
+  });
